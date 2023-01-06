20699018d9e25457aa0f6048de1912e6218260b4
LeDrew2017 <ledrew101806@gmail.com>
2023-01-06 10:06:40 -0500

    added elixir rom

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_d1.mk
+    $(LOCAL_DIR)/elixir_d1.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_d1-eng \
-    xtended_d1-user \
-    xtended_d1-userdebug
+    elixir_d1-eng \
+    elixir_d1-user \
+    elixir_d1-userdebug
